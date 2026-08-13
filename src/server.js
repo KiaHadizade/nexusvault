5b4a70e0215cfa59fc138f7a20832a7f312193ec
Kia Hadizade <kiahadizade3077@gmail.com>
2026-08-14 00:19:36 +0330

    feat(express): Add environment and configuration

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import app from "./app.js" // Importing the Express application
+import "dotenv/config" // Load the environment
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`)
