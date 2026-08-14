4276018557fe4d4bd2e441fcb26c21622abc9d98
Kia Hadizade <kiahadizade3077@gmail.com>
2026-08-14 15:40:08 +0330

    feat(express): Added dotenv

--- src/app.js
+++ src/app.js
@@ -6,6 +6,9 @@ app.use(express.json()) // JSON middleware
 
 // Route
 app.get("/api/health", (req, res) => {
+    console.log(req.method)
+    console.log(req.url)
+    
     res.json({
         status: "ok",
         message: "Cloud Storage API is running"
--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import app from "./app.js" // Importing the Express application
 import "dotenv/config" // Load the environment
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000 // Use the environment's PORT if it exists; otherwise use 5000
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`)
