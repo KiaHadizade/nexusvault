7413a84d6ba247a0a77139b038b8fd2c3fd01946
Kia Hadizade <kiahadizade3077@gmail.com>
2026-08-15 13:01:01 +0330

    Added error handler

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import healthRouter from "./routes/health.routes.js"
 import requestLogger from "./middleware/logger.middleware.js"
+import errorHandler from "./middleware/error.middleware.js"
 
 const app = express() // Backend application
 
@@ -10,5 +11,7 @@ app.use(requestLogger)
 
 // Route
 app.use("/api/health", healthRouter)
+
+app.use(errorHandler) // The error handler is placed after routes
 
 export default app // Export express application
