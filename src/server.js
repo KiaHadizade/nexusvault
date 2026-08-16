ab8b8f7c7f6a130b93850b2bcf92cc42dd86ebbf
Kia Hadizade <kiahadizade3077@gmail.com>
2026-08-16 12:54:42 +0330

    Add top-level error handler

--- src/server.js
+++ src/server.js
@@ -5,11 +5,17 @@ import connectDatabase from "./config/database.js"
 const PORT = process.env.PORT || 5000 // Use the environment's PORT if it exists; otherwise use 5000
 
 const startServer = async () => {
-    await connectDatabase()
+    try {
+        await connectDatabase()
 
-    app.listen(PORT, () => {
-        console.log(`Server running on http://localhost:${PORT}`)
-    })
+        app.listen(PORT, () => {
+            console.log(`Server running on http://localhost:${PORT}`)
+        })
+        
+    } catch (error) {
+        console.error("Failed to start server:", error)
+        process.exit(1)
+    }
 }
 
 startServer()
