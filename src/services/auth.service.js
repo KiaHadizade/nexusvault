5b1615868505e83097e5cc0e011e16c2ffef9399
Kia Hadizade <kiahadizade3077@gmail.com>
2026-08-18 01:03:19 +0330

    Create JWT functions

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,11 +1,27 @@
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 const SALT_ROUNDS = 12 // Controls the computational cost
 
+// Password Hashing
 export const hashPassword = async (password) => {
     return bcrypt.hash(password, SALT_ROUNDS)
 }
 
+// Password Comparison
 export const comparePassword = async (password, passwordHash) => {
     return bcrypt.compare(password, passwordHash)
+}
+
+// Generate Token
+export const generateToken = (userId) => {
+    return jwt.sign(
+        {
+            userId
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: process.env.JWT_EXPIRES_IN || "1h"
+        }
+    )
 }
