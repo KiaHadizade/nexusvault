4a8dbdac1b4cadb870ba52d51f4c9cd340478cd4
Kia Hadizade <kiahadizade3077@gmail.com>
2026-08-19 01:53:17 +0330

    Add delete route

--- src/routes/file.routes.js
+++ src/routes/file.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express"
 import authenticate from "../middleware/auth.middleware.js"
 import upload from "../middleware/upload.middleware.js"
-import { uploadFile, listFiles, downloadFile } from "../controllers/file.controller.js"
+import { uploadFile, listFiles, downloadFile, deleteFile } from "../controllers/file.controller.js"
 
 const router = Router()
 
 router.get("/", authenticate, listFiles)
 router.post("/upload", authenticate, upload.single("file"), uploadFile)
 router.get("/:id/download", authenticate, downloadFile)
+router.delete("/:id", authenticate, deleteFile)
 
 export default router
