3a681cc5f5650e054c20f32402f6cd1593cfac20
Kia Hadizade <kiahadizade3077@gmail.com>
2026-08-18 11:42:18 +0330

    Add test user registeration

--- src/controllers/health.controller.js
+++ src/controllers/health.controller.js
@@ -1,3 +1,5 @@
+import User from "../models/user.model.js"
+
 // Handle GET health request and generate the response
 export const getHealth = (req, res) => {
     res.json({
@@ -5,4 +7,21 @@ export const getHealth = (req, res) => {
         message: "Cloud Storage API is running",
         requestTime: req.requestTime
     })
+}
+
+export const createTestUser = async (req, res, next) => {
+    try {
+        const user = await User.create({
+            email: "test@example.com",
+            password: "temporary-password"
+        })
+
+        res.status(201).json({
+            id: user._id,
+            email: user.email
+        })
+
+    } catch (error) {
+        next(error)
+    }
 }
--- src/routes/health.routes.js
+++ src/routes/health.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express"
-import { getHealth } from "../controllers/health.controller.js"
+import { getHealth, createTestUser } from "../controllers/health.controller.js"
 
 const router = Router() // Mini Express application dedicated to a group of routes
 
 router.get("/", getHealth)
+router.post("/test-user", createTestUser)
 
 router.post("/test", (req, res) => {
     console.log(req.body)
