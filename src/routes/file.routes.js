c5c367e0d1f7c0dfc3babe30d8a6dd5655cbc636
Kia Hadizade <kiahadizade3077@gmail.com>
2026-08-19 01:17:36 +0330

    Create file routes

---
+++ src/routes/file.routes.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import authenticate from "../middleware/auth.middleware.js";
+import upload from "../middleware/upload.middleware.js";
+import { uploadFile } from "../controllers/file.controller.js";
+
+const router = Router()
+
+router.post(
+    "/upload",
+    authenticate,
+    upload.single("file"),
+    uploadFile
+)
+
+export default router
