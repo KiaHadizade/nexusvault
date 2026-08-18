feed438015b675b2d08ebbff1adbbe9d8cc768cc
Kia Hadizade <kiahadizade3077@gmail.com>
2026-08-19 01:45:27 +0330

    Add download route

--- src/routes/file.routes.js
+++ src/routes/file.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express"
 import authenticate from "../middleware/auth.middleware.js"
 import upload from "../middleware/upload.middleware.js"
-import { uploadFile, listFiles } from "../controllers/file.controller.js"
+import { uploadFile, listFiles, downloadFile } from "../controllers/file.controller.js"
 
 const router = Router()
 
 router.get("/", authenticate, listFiles)
 router.post("/upload", authenticate, upload.single("file"), uploadFile)
+router.get("/:id/download", authenticate, downloadFile)
 
 export default router
