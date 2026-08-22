57f0bfe17fa6aee9cde43a14c716f88d5eb3a62e
Kia Hadizade <kiahadizade3077@gmail.com>
2026-08-22 11:21:38 +0330

    Add shared file download route

--- src/routes/share.routes.js
+++ src/routes/share.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express"
-import { createShare } from "../controllers/share.controller.js"
+import { createShare, downloadSharedFile } from "../controllers/share.controller.js"
 import authMiddleware from "../middleware/auth.middleware.js"
 
 const router = Router()
 
 router.post("/:id", authMiddleware, createShare)
-
+router.get("/:token", downloadSharedFile)
 export default router
