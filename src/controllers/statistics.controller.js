33ab0f542dad0b14bca85cf776a1d93db73ecd11
Kia Hadizade <kiahadizade3077@gmail.com>
2026-08-23 01:26:08 +0330

    Create the statistics controller

---
+++ src/controllers/statistics.controller.js
@@ -0,0 +1,32 @@
+import { getFileStatistics, getShareStatistics } from "../services/statistics.service.js"
+
+export const getStatistics = async (req, res, next) => {
+    try {
+        const userId = req.user.id
+
+        const [fileStats, shareStats] = await Promise.all([
+            getFileStatistics(userId),
+            getShareStatistics(userId)
+        ])
+
+        res.json({
+            files: {
+                total: fileStats.totalFiles,
+                totalSize: fileStats.totalSize
+            },
+
+            shares: {
+                total: shareStats.totalShares,
+                active: shareStats.activeShares,
+                revoked: shareStats.revokedShares,
+                expired: shareStats.expiredShares
+            },
+
+            downloads: {
+                total: shareStats.totalDownloads
+            }
+        })
+    } catch (error) {
+        next(error)
+    }
+}
