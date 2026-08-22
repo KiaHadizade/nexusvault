38fea2d1821b49c5929b35e2f8c972ea0eb4e973
Kia Hadizade <kiahadizade3077@gmail.com>
2026-08-22 23:42:45 +0330

    Add revoke share route

--- src/routes/share.routes.js
+++ src/routes/share.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express"
-import { createShare, downloadSharedFile } from "../controllers/share.controller.js"
+import { createShare, downloadSharedFile, revokeShare } from "../controllers/share.controller.js"
 import authMiddleware from "../middleware/auth.middleware.js"
 
 const router = Router()
 
 router.post("/:id", authMiddleware, createShare)
 router.get("/:token", downloadSharedFile)
+router.delete("/revoke/:id", authMiddleware, revokeShare)
+
 export default router
